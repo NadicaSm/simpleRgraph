4f1017e69f7f796f1f250ea35c71238fc9ed6ae9
Nadica Miljković <41929315+NadicaSm@users.noreply.github.com>
2020-04-05 13:58:09 +0200

    Update popularR.R

--- popularR.R
+++ popularR.R
@@ -2,15 +2,17 @@
 # Copyright (C) 2017 The R Foundation for Statistical Computing
 # Platform: x86_64-w64-mingw32/x64 (64-bit)
 
+# update 1: on April 5, 2020 to add novel number
+
 library(ggplot2)
 
 # dates when number of CRAN packages was checked
-dates = c('2017-02-10', '2018-02-17', '2019-02-20')
+dates = c('2017-02-10', '2018-02-17', '2019-02-20', '2020-04-05')
 dates <- as.Date(dates)
 dates
 
 # number of packages corresponding to dates
-numberOfPackages = c(10065, 12162, 13741)
+numberOfPackages = c(10065, 12162, 13741, 15514)
 
 # creating data frame
 d = list()
@@ -22,7 +24,7 @@ d
 # plotting bar graph
 p <- ggplot(data=d, aes(x = dates, y = numberOfPackages)) +
   geom_bar(stat="identity")
-p + coord_flip(ylim = c(0, 15000)) + theme_minimal() +
+p + coord_flip(ylim = c(0, 16000)) + theme_minimal() +
   scale_y_discrete(limits = numberOfPackages) +
   ggtitle('CRAN packages') + ylab('number of packages') + xlab('year')
 
