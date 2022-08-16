5cfa9adf4de6dcea5e8da0363c201a788a16aec5
Nadica Miljković <41929315+NadicaSm@users.noreply.github.com>
2022-08-16 12:37:54 +0200

    Update popularR.R

--- popularR.R
+++ popularR.R
@@ -2,17 +2,18 @@
 # Copyright (C) 2017 The R Foundation for Statistical Computing
 # Platform: x86_64-w64-mingw32/x64 (64-bit)
 
-# update 1: on April 5, 2020 to add novel number
+# update 1: on April 5, 2020 to add novel number of contributed packages from https://cran.r-project.org/web/packages/
+# update 2: on August 16, 2022 to add missing numbers
 
 library(ggplot2)
 
 # dates when number of CRAN packages was checked
-dates = c('2017-02-10', '2018-02-17', '2019-02-20', '2020-04-05')
+dates = c('2017-02-10', '2018-02-17', '2019-02-20', '2020-04-05', '2021-03-01', '2022-08-16')
 dates <- as.Date(dates)
 dates
 
 # number of packages corresponding to dates
-numberOfPackages = c(10065, 12162, 13741, 15514)
+numberOfPackages = c(10065, 12162, 13741, 15514, 17214, 18442)
 
 # creating data frame
 d = list()
